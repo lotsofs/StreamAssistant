690760694233d26b521104da23ebc57b3529da4b
S <contact@lotsofs.com>
2018-08-05 22:32:01 +0200

    Program now adds a bar at the bottom to assist OBS with right alignment, since OBS cant do it properly itself

--- StreamAssistant2/EventLog.cs
+++ StreamAssistant2/EventLog.cs
@@ -31,6 +31,7 @@ namespace StreamAssistant2
 		string[] events;	// circular array
 		int nextEventIndex;
 
+		readonly string forceWidthBar = "==================";
 		readonly string[] logFormats = new string[] {
 			"{0}  SUB X{1}",	
 			"{0}  {1} BITS",		
@@ -79,6 +80,7 @@ namespace StreamAssistant2
 				output += events[j];
 				output += Environment.NewLine;
 			}
+			output += forceWidthBar;
 			File.WriteAllText(Path, output);
 		}
 
