638d559ee8f23741a8f1a49eefe97726721cc588
S <18078548+lotsofs@users.noreply.github.com>
2024-03-15 02:12:46 +0100

    'Upgrade', whatever that means. But now I can C# 8.0

--- StreamAssistant2/Form_StreamAssistant.Designer.cs
+++ StreamAssistant2/Form_StreamAssistant.Designer.cs
@@ -26,128 +26,44 @@
 		/// </summary>
 		private void InitializeComponent() {
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_StreamAssistant));
-			this.sceneManager_groupBox = new System.Windows.Forms.GroupBox();
-			this.sceneManager_ButtonSuspend0 = new System.Windows.Forms.Button();
-			this.sceneManager_ButtonSuspend720 = new System.Windows.Forms.Button();
-			this.sceneManager_ButtonSuspend60 = new System.Windows.Forms.Button();
-			this.sceneManager_ButtonSuspend5 = new System.Windows.Forms.Button();
-			this.sceneManager_Label = new System.Windows.Forms.Label();
-			this.sceneManager_EnabledCheckBox = new System.Windows.Forms.CheckBox();
-			this.sceneManager_TextBox = new System.Windows.Forms.TextBox();
-			this.sceneManager_groupBox.SuspendLayout();
+			this.buttonEnable = new System.Windows.Forms.Button();
+			this.backgroundWorkerTcp = new System.ComponentModel.BackgroundWorker();
 			this.SuspendLayout();
 			// 
-			// sceneManager_groupBox
+			// buttonEnable
 			// 
-			this.sceneManager_groupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.sceneManager_groupBox.Controls.Add(this.sceneManager_TextBox);
-			this.sceneManager_groupBox.Controls.Add(this.sceneManager_ButtonSuspend0);
-			this.sceneManager_groupBox.Controls.Add(this.sceneManager_ButtonSuspend720);
-			this.sceneManager_groupBox.Controls.Add(this.sceneManager_ButtonSuspend60);
-			this.sceneManager_groupBox.Controls.Add(this.sceneManager_ButtonSuspend5);
-			this.sceneManager_groupBox.Controls.Add(this.sceneManager_Label);
-			this.sceneManager_groupBox.Controls.Add(this.sceneManager_EnabledCheckBox);
-			this.sceneManager_groupBox.Location = new System.Drawing.Point(12, 12);
-			this.sceneManager_groupBox.Name = "sceneManager_groupBox";
-			this.sceneManager_groupBox.Size = new System.Drawing.Size(261, 158);
-			this.sceneManager_groupBox.TabIndex = 3;
-			this.sceneManager_groupBox.TabStop = false;
-			this.sceneManager_groupBox.Text = "Scene Manager";
+			this.buttonEnable.Font = new System.Drawing.Font("Sofia Sans Semi Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonEnable.Location = new System.Drawing.Point(798, 12);
+			this.buttonEnable.Name = "buttonEnable";
+			this.buttonEnable.Size = new System.Drawing.Size(121, 61);
+			this.buttonEnable.TabIndex = 0;
+			this.buttonEnable.Text = "Enable";
+			this.buttonEnable.UseVisualStyleBackColor = true;
+			this.buttonEnable.Click += new System.EventHandler(this.buttonEnable_Click);
 			// 
-			// sceneManager_ButtonSuspend0
+			// backgroundWorkerTcp
 			// 
-			this.sceneManager_ButtonSuspend0.Location = new System.Drawing.Point(83, 19);
-			this.sceneManager_ButtonSuspend0.Name = "sceneManager_ButtonSuspend0";
-			this.sceneManager_ButtonSuspend0.Size = new System.Drawing.Size(172, 23);
-			this.sceneManager_ButtonSuspend0.TabIndex = 6;
-			this.sceneManager_ButtonSuspend0.Text = "Continue / Next";
-			this.sceneManager_ButtonSuspend0.UseVisualStyleBackColor = true;
-			this.sceneManager_ButtonSuspend0.Click += new System.EventHandler(this.sceneManager_ButtonSuspend0_Click);
-			// 
-			// sceneManager_ButtonSuspend720
-			// 
-			this.sceneManager_ButtonSuspend720.Location = new System.Drawing.Point(83, 106);
-			this.sceneManager_ButtonSuspend720.Name = "sceneManager_ButtonSuspend720";
-			this.sceneManager_ButtonSuspend720.Size = new System.Drawing.Size(172, 23);
-			this.sceneManager_ButtonSuspend720.TabIndex = 5;
-			this.sceneManager_ButtonSuspend720.Text = "Suspend for 720 minutes";
-			this.sceneManager_ButtonSuspend720.UseVisualStyleBackColor = true;
-			this.sceneManager_ButtonSuspend720.Click += new System.EventHandler(this.sceneManager_ButtonSuspend720_Click);
-			// 
-			// sceneManager_ButtonSuspend60
-			// 
-			this.sceneManager_ButtonSuspend60.Location = new System.Drawing.Point(83, 77);
-			this.sceneManager_ButtonSuspend60.Name = "sceneManager_ButtonSuspend60";
-			this.sceneManager_ButtonSuspend60.Size = new System.Drawing.Size(172, 23);
-			this.sceneManager_ButtonSuspend60.TabIndex = 4;
-			this.sceneManager_ButtonSuspend60.Text = "Suspend for 60 minutes";
-			this.sceneManager_ButtonSuspend60.UseVisualStyleBackColor = true;
-			this.sceneManager_ButtonSuspend60.Click += new System.EventHandler(this.sceneManager_ButtonSuspend60_Click);
-			// 
-			// sceneManager_ButtonSuspend5
-			// 
-			this.sceneManager_ButtonSuspend5.Location = new System.Drawing.Point(83, 48);
-			this.sceneManager_ButtonSuspend5.Name = "sceneManager_ButtonSuspend5";
-			this.sceneManager_ButtonSuspend5.Size = new System.Drawing.Size(172, 23);
-			this.sceneManager_ButtonSuspend5.TabIndex = 3;
-			this.sceneManager_ButtonSuspend5.Text = "Suspend for 5 minutes";
-			this.sceneManager_ButtonSuspend5.UseVisualStyleBackColor = true;
-			this.sceneManager_ButtonSuspend5.Click += new System.EventHandler(this.sceneManager_ButtonSuspend5_Click);
-			// 
-			// sceneManager_Label
-			// 
-			this.sceneManager_Label.AutoSize = true;
-			this.sceneManager_Label.Location = new System.Drawing.Point(6, 90);
-			this.sceneManager_Label.Name = "sceneManager_Label";
-			this.sceneManager_Label.Size = new System.Drawing.Size(47, 13);
-			this.sceneManager_Label.TabIndex = 1;
-			this.sceneManager_Label.Text = "Override";
-			// 
-			// sceneManager_EnabledCheckBox
-			// 
-			this.sceneManager_EnabledCheckBox.AutoSize = true;
-			this.sceneManager_EnabledCheckBox.Location = new System.Drawing.Point(6, 19);
-			this.sceneManager_EnabledCheckBox.Name = "sceneManager_EnabledCheckBox";
-			this.sceneManager_EnabledCheckBox.Size = new System.Drawing.Size(59, 17);
-			this.sceneManager_EnabledCheckBox.TabIndex = 0;
-			this.sceneManager_EnabledCheckBox.Text = "Enable";
-			this.sceneManager_EnabledCheckBox.UseVisualStyleBackColor = true;
-			this.sceneManager_EnabledCheckBox.CheckedChanged += new System.EventHandler(this.sceneManager_EnabledCheckBox_CheckedChanged);
-			// 
-			// sceneManager_TextBox
-			// 
-			this.sceneManager_TextBox.Location = new System.Drawing.Point(6, 106);
-			this.sceneManager_TextBox.Name = "sceneManager_TextBox";
-			this.sceneManager_TextBox.Size = new System.Drawing.Size(71, 20);
-			this.sceneManager_TextBox.TabIndex = 7;
-			this.sceneManager_TextBox.TextChanged += new System.EventHandler(this.sceneManager_TextBox_TextChanged);
+			this.backgroundWorkerTcp.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerTcp_DoWork);
 			// 
 			// Form_StreamAssistant
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(285, 182);
-			this.Controls.Add(this.sceneManager_groupBox);
+			this.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.ClientSize = new System.Drawing.Size(931, 573);
+			this.Controls.Add(this.buttonEnable);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form_StreamAssistant";
 			this.Text = "Stream Assistant";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_StreamAssistant_FormClosing);
-			this.sceneManager_groupBox.ResumeLayout(false);
-			this.sceneManager_groupBox.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-		private System.Windows.Forms.GroupBox sceneManager_groupBox;
-		private System.Windows.Forms.Label sceneManager_Label;
-		private System.Windows.Forms.CheckBox sceneManager_EnabledCheckBox;
-		private System.Windows.Forms.Button sceneManager_ButtonSuspend5;
-		private System.Windows.Forms.Button sceneManager_ButtonSuspend60;
-		private System.Windows.Forms.Button sceneManager_ButtonSuspend720;
-		private System.Windows.Forms.Button sceneManager_ButtonSuspend0;
-		private System.Windows.Forms.TextBox sceneManager_TextBox;
+
+		private System.Windows.Forms.Button buttonEnable;
+		private System.ComponentModel.BackgroundWorker backgroundWorkerTcp;
 	}
 }
 
--- StreamAssistant2/Form_StreamAssistant.cs
+++ StreamAssistant2/Form_StreamAssistant.cs
@@ -11,19 +11,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections.Specialized;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.Versioning;
 
 namespace StreamAssistant2
 {
 	public partial class Form_StreamAssistant : Form, ISaveable
 	{
-		SceneManager _sceneManager;
+		static bool _tcpEnabled;
+		IPAddress _ipAddress;
+		int _port = 49152;
+		StreamReader _str;
+		StreamWriter _stw;
+
+		TcpClient _client;
+		TcpListener _listener;
 
 		#region opening closing
 
 		public Form_StreamAssistant() {
 			InitializeComponent();
-
-			_sceneManager = new SceneManager();
 
 			LoadSettings();
 		}
@@ -45,49 +53,68 @@ namespace StreamAssistant2
 		/// Loads settings
 		/// </summary>
 		public void LoadSettings() {
-			SaveLoad.OnSave += SaveSettings;
+			//SaveLoad.OnSave += SaveSettings;
 
-			NameValueCollection appSettings = ConfigurationManager.AppSettings;
+			//NameValueCollection appSettings = ConfigurationManager.AppSettings;
 
-			sceneManager_EnabledCheckBox.Checked = bool.Parse(appSettings["SceneManagerEnabled"]);
-			_sceneManager.Enabled = sceneManager_EnabledCheckBox.Checked;
+			//sceneManager_EnabledCheckBox.Checked = bool.Parse(appSettings["SceneManagerEnabled"]);
+			//_sceneManager.Enabled = sceneManager_EnabledCheckBox.Checked;
 		}
 
 		/// <summary>
 		/// Save settings to file
 		/// </summary>
 		public void SaveSettings() {
-			Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			//Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
-			config.AppSettings.Settings["SceneManagerEnabled"].Value = sceneManager_EnabledCheckBox.Checked.ToString();
+			//config.AppSettings.Settings["SceneManagerEnabled"].Value = sceneManager_EnabledCheckBox.Checked.ToString();
 
-			config.Save();
+			//config.Save();
 		}
 
 		#endregion
 
-		private void sceneManager_EnabledCheckBox_CheckedChanged(object sender, EventArgs e) {
-			_sceneManager.Enabled = sceneManager_EnabledCheckBox.Checked;
+		private void buttonEnable_Click(object sender, EventArgs e) {
+			buttonEnable.Text = "Enabled";
+			buttonEnable.Enabled = false;
+			_ipAddress = new IPAddress(0x0100007f);
+			_listener = new TcpListener(_ipAddress, _port);
+			_listener.Start();
+			_tcpEnabled = true;
+			backgroundWorkerTcp.RunWorkerAsync();
 		}
 
-		private void sceneManager_ButtonSuspend5_Click(object sender, EventArgs e) {
-			_sceneManager.Suspend(300);
-		}
+		//private async void backgroundWorkerTcp_DoWork(object sender, DoWorkEventArgs e) {
+		private void backgroundWorkerTcp_DoWork(object sender, DoWorkEventArgs e) {
+			while (_tcpEnabled) {
+				this.buttonEnable.Invoke(new MethodInvoker(delegate () {
+					buttonEnable.Text = "Waiting";
+				}));
+				_client = _listener.AcceptTcpClient();
 
-		private void sceneManager_ButtonSuspend60_Click(object sender, EventArgs e) {
-			_sceneManager.Suspend(3600);
-		}
+				_str = new StreamReader(_client.GetStream());
+				_stw = new StreamWriter(_client.GetStream());
+				_stw.AutoFlush = true;
 
-		private void sceneManager_ButtonSuspend720_Click(object sender, EventArgs e) {
-			_sceneManager.Suspend(43200);
-		}
-
-		private void sceneManager_ButtonSuspend0_Click(object sender, EventArgs e) {
-			_sceneManager.Suspend(0);
-		}
-
-		private void sceneManager_TextBox_TextChanged(object sender, EventArgs e) {
-			_sceneManager.OverrideGame = sceneManager_TextBox.Text;
+				if (_client.Connected) {
+					buttonEnable.Invoke(new MethodInvoker(delegate () {
+						buttonEnable.Text = "Reading";
+					}));
+					try {
+						string? s = _str.ReadLine();
+						// do something with the received information
+						_client.GetStream().Close();
+						_client.Close();
+					}
+					catch (Exception ex) {
+						MessageBox.Show(ex.Message.ToString());
+					}
+				}
+			}
+			buttonEnable.Invoke(new MethodInvoker(delegate () {
+				buttonEnable.Enabled = true;
+				buttonEnable.Text = "Reading";
+			}));
 		}
 	}
 }
